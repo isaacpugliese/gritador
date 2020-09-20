d3c646fc52a5709a4ebb1d8c10747c13a6d18d70
isaac pugliese <isaacpugliese2010@gmail.com>
2020-09-20 15:28:31 -0300

    gritador finalizado

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite uma Frase: ");
+            string Frase = Console.ReadLine();
+            Console.Clear();
+            Console.Write(Frase.ToUpper()+"!");           
+        }
+    }
+}
